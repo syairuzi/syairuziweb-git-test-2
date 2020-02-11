7d3e5946511a02b03eee9496264a45e986275de8
syairuzi <syairuzi99@gmail.com>
2020-02-11 14:54:23 +0700

    Menambahkan list

--- index.php
+++ index.php
@@ -10,6 +10,10 @@
 <body>
     <h1>Hello world</h1>
     <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Incidunt repellat doloribus nihil facere tempora quia reiciendis corrupti ipsam laudantium molestias, suscipit omnis, cumque odit voluptatem non eveniet id vitae? Laudantium.</p>
+    <ul>
+        <li>Laravel</li>
+        <li>Vue JS</li>
+    </ul>
 </body>
 
 </html>
